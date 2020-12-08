690752e7ee4d321cb00853ed7db2f1eebdf0f8d1
Jon Finger <jaefinger@gmail.com>
2020-12-08 11:53:19 -0600

    Added twitter credentials

--- augie.py
+++ augie.py
@@ -1,3 +1,7 @@
+# Augie Bot v0.0
+# Author: JAEFinger
+
+# Package Imports
 import os
 import tweepy
 import pandas
@@ -5,6 +9,18 @@ import numpy
 
 from dotenv import load_dotenv
 load_dotenv()
+
+# Load environment variables
+API_KEY = os.getenv("API_KEY")
+API_KEY_SECRET = os.getenv("API_KEY_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+# try:
+#     api.verify_credentials()
+#     print("Authentication OK")
+# except:
+#     print("Error during authentication")
 
 # if __name__ == '__main__':
 print('woof!')
