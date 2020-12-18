30b6c4f3aa78a7df6ff0ed1371da1d3e04607cfb
Jon Finger <jaefinger@gmail.com>
2020-12-18 11:16:04 -0600

    Fixed twitter authentication and can download TL

--- augie.py
+++ augie.py
@@ -11,20 +11,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load environment variables
-API_KEY = os.getenv("API_KEY")
-API_KEY_SECRET = os.getenv("API_KEY_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+consumer_key = os.getenv("TW_CONS_KEY")
+consumer_secret = os.getenv("TW_CONS_SEC")
+access_token = os.getenv("TW_ACCESS_TOKEN")
+access_token_secret = os.getenv("TW_ACCESS_TOKEN_SECRET")
 
-auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# Set authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+# Instantiate tweepy api
+api = tweepy.API(auth)
 
-# if __name__ == '__main__':
-# print('woof!')
+# Get public tweets
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
