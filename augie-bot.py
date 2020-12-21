35537cac4ef4bcf3e0dd7682821797c9b1dba1d3
Jon Finger <jaefinger@gmail.com>
2020-12-21 12:32:12 -0600

    Cleaned up main script

--- augie-bot.py
+++ augie-bot.py
@@ -1,6 +1,7 @@
 # Augie Bot v0.0
 # Author: JAEFinger
 
+
 # Package Imports
 import os
 import tweepy
@@ -8,15 +9,14 @@ import pandas
 import numpy
 import random
 
-# Custom features
-
+# Import Custom features
 from features.augie_bark import AugieBark
 
-# Environmental Variables
+# Load Environmental Variables
 from dotenv import load_dotenv
 load_dotenv()
 
-# Load environment variables
+# Create credentials from environmental variables
 consumer_key = os.getenv("TW_CONS_KEY")
 consumer_secret = os.getenv("TW_CONS_SEC")
 access_token = os.getenv("TW_ACCESS_TOKEN")
@@ -29,14 +29,6 @@ auth.set_access_token(access_token, access_token_secret)
 # Instantiate tweepy api
 api = tweepy.API(auth)
 
-# Get public tweets
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-
-
 if __name__ == "__main__":
-    tweet_quote()
-
-# Randomly bark
-AugieBark(api)
+    # Randomly bark
+    AugieBark(api)
