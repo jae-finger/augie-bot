4cb7f4cd3a23d80b31263542620f68c7c7e7b1e3
Jon Finger <jaefinger@gmail.com>
2021-01-01 08:32:28 -0500

    Added additional augie facts.

--- features/augie_facts.py
+++ features/augie_facts.py
@@ -19,14 +19,19 @@ def AugieFact(consumer_key, consumer_secret, access_token, access_token_secret):
 
     # Create list of augie facts
     augie_facts = [
-        'Hello -- my name is Augie the doggie.',
-        'I am an apricot purebred miniature poodle.',
+        'Hello -- my name is Augie the doggy.',
+        'I am an apricot, purebred miniature poodle.',
         'My favorite toy is my stuffed sheep.',
         'I am six months old!',
         'My favorite treats are my bison burgs.',
         "My nickname is 'poof head'.",
         'I enjoy going to obedience class each week.',
-        'My best friends are Cosmo and Siri.'
+        'My best friends are Cosmo and Siri.',
+        'I realllllly love walks.',
+        'I spend most of my days sleeping on the couch.',
+        'My favorite treats are bison burgers.',
+        'I prefer winter over summer; walks are so much cooler then.',
+        'For some reason, everybody thinks I am a goldendoodle.'
     ]
 
     interval = 4
